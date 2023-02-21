8e94a240aa36c28fd601c0cdeae4c6a40b1a6898
Alex-Der-coder <110197972+Alex-Der-coder@users.noreply.github.com>
2023-02-21 17:43:38 +0100

    sanitariz app use for auth

--- back/app.js
+++ back/app.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const helmet = require("helmet");
+const helmet = require('helmet');
 const path = require('path');
 const app = express();
 const cors = require('cors');
-/* Routes api */
+const expressSanitizer = require('express-sanitizer');
 const userRoutes = require('./routes/user');
 const sauceRoutes = require('./routes/sauce');
 
 
 /* Mongo DB Connexion */
-mongoose.connect('mongodb+srv://Alex:ebhrSVu6PVAIiaQ0@atlascluster.yhbyxcr.mongodb.net/?retryWrites=true&w=majority',{ useNewUrlParser: true,useUnifiedTopology: true })
-.then(() => console.log('Connexion à MongoDB réussie !'))
-.catch(() => console.log('Connexion à MongoDB échouée !'));
+mongoose.connect('mongodb+srv://Alex:ebhrSVu6PVAIiaQ0@atlascluster.yhbyxcr.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
 /* Middleware CORS policy */
 app.use(cors());
@@ -25,12 +25,13 @@ app.use(helmet());
 app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
 
 /* Middleware image */
-app.use('/images', express.static(path.join(__dirname, 'images'))); // gestion images de manière statiques
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
+/* Middleware for data sanitization */
+app.use(expressSanitizer());
 
 /* Route API */
 app.use('/api/auth', userRoutes);
 app.use('/api', sauceRoutes);
-
 
 module.exports = app;
