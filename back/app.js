df5e41ccff61caacbc901c9e46898a5d06fb0bdb
Alexandre De Roeck <Mr.Alexandre.deroeck@gmail.comm>
2023-02-27 14:11:08 +0100

    front-server debug

---
+++ back/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const path = require('path');
+const app = express();
+const cors = require('cors');
+const expressSanitizer = require('express-sanitizer');
+const userRoutes = require('./routes/user');
+const sauceRoutes = require('./routes/sauce');
+
+
+/* Mongo DB Connexion */
+mongoose.connect('mongodb+srv://Alex:ebhrSVu6PVAIiaQ0@atlascluster.yhbyxcr.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+/* Middleware CORS policy */
+app.use(cors());
+
+/* Convert all response and get in json data */
+app.use(express.json());
+
+/* Helmet */
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+
+/* Middleware image */
+app.use('/images', express.static(path.join(__dirname, 'images')));
+
+/* Middleware for data sanitization */
+app.use(expressSanitizer());
+/*
+
+/* Route API */
+app.use('/api/auth', userRoutes);
+app.use('/api', sauceRoutes);
+
+module.exports = app;
---
+++ back/routes/user.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const expressSanitizer = require('express-sanitizer');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const Router = express.Router();
+const User = require('../models/userModel');
+
+const app = express();
+
+// Middleware pour la désinfection de données
+app.use(expressSanitizer());
+
+/* Register an account */
+exports.signup = (req, res, next) => {
+    // Désinfecte l'email et le mot de passe de l'utilisateur
+    const email = req.sanitize(req.body.email);
+    const password = req.sanitize(req.body.password);
+
+    bcrypt.hash(password, 10)
+    .then(hash => {
+        const user = new User({ email, password: hash });
+        user.save()
+        .then(() => res.status(200).json({ message: 'User created' }))
+        .catch(error => res.status(400).json({ message: 'address mail already used' }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+/* Login */
+exports.login = (req, res, next) => {
+    // Désinfecte l'email et le mot de passe de l'utilisateur
+    const email = req.sanitize(req.body.email);
+    const password = req.sanitize(req.body.password);
+
+    User.findOne({ email })
+    .then(user => {
+        if (!user) {
+            return res.status(401).json({ message: 'User not found' });
+        }
+
+        bcrypt.compare(password, user.password)
+        .then(valid => {
+            if (!valid) {
+                return res.status(401).json({ message: 'Error on email or password' });
+            }
+
+            res.status(200).json({
+                userId: user._id,
+                token: jwt.sign(
+                    { userId: user._id },
+                    'RANDOM_TOKEN_SECRET',
+                    { expiresIn: '24h' }
+                )
+            });
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
+};
+
+module.exports = Router ;
